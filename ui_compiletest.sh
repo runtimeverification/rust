a84768f7c9021055abb67b1328744a379944b739
Stephen Skeirik <stephen.skeirik@runtimeverification.com>
2024-06-06 20:11:28 -0500

    improve compiletest script usage message

--- ui_compiletest.sh
+++ ui_compiletest.sh
@@ -2,23 +2,31 @@
 
 set -eu
 
-USAGE="$0 <rustc_dir> <out_dir> <source_file> [args...]\nset env var RUST_BIN to custom rustc if desired"
-function die() { printf "%s\n" "error: $*"; exit 1; }
+USAGE=\
+"$0 <rustc_dir> <out_dir> <source_file> [opts...]\\n\\n"\
+"common options:\\n\\n"\
+"  --force-rerun\\t\\t\\t- runs tests, even when outputs exist\\n"\
+"  --ignored\\t\\t\\t- run all tests, even ignored tests\\n"\
+"  --pass <check|build|run>\\t- runs tests at given mode\\n"\
+"  --help\\t\\t\\t- see all options (note: most are defaulted)\\n"\
+"\\nset env var RUST_BIN to custom rustc if desired"
+
+function die() { echo -e "$*"; exit 1; }
 
 [ $# -lt 3 ] && die "$USAGE"
 RUST_DIR=$1
 OUT_DIR=$2
 SOURCE_FILE=$3
 shift 3
-[ ! -d "$RUST_DIR" ] && die "$USAGE\n<rustc_dir=$RUST_DIR> must be a valid directory"
-mkdir -p "$OUT_DIR" 2>/dev/null || die "$USAGE\nunable to create <out_dir=$OUT_DIR>"
-[ ! -f "$SOURCE_FILE" ] && die "$USAGE\nunable to find <source_file=$SOURCE_FILE>"
+[ ! -d "$RUST_DIR" ] && die "$USAGE\nerror: <rustc_dir=$RUST_DIR> must be a valid directory"
+mkdir -p "$OUT_DIR" 2>/dev/null || die "$USAGE\nerror: unable to create <out_dir=$OUT_DIR>"
+[ ! -f "$SOURCE_FILE" ] && die "$USAGE\nerror: unable to find <source_file=$SOURCE_FILE>"
 
-[ command -v rustc 2>/dev/null ] && die "$USAGE\nrustc binary must be available on PATH"
+[ command -v rustc 2>/dev/null ] && die "$USAGE\nerror: rustc binary must be available on PATH"
 ARCH=$(rustc -vV | grep '^host' | grep -o '[^: ]*$')
 
 [ -z "${RUST_BIN+x}" ] && RUST_BIN="$RUST_DIR/build/$ARCH/stage1/bin/rustc"
-[ ! -x "$RUST_BIN" ] && die "$USAGE\nenv var RUST_BIN or default stage1 rustc does not point to executable"
+[ ! -x "$RUST_BIN" ] && die "$USAGE\nerror: env var RUST_BIN or default stage1 rustc does not point to executable"
 
 LLVM_COMPONENTS="aarch64 aarch64asmparser aarch64codegen aarch64desc aarch64disassembler aarch64info aarch64utils aggressiveinstcombine all all-targets analysis arm armasmparser armcodegen armdesc armdisassembler arminfo armutils asmparser asmprinter avr avrasmparser avrcodegen avrdesc avrdisassembler avrinfo binaryformat bitreader bitstreamreader bitwriter bpf bpfasmparser bpfcodegen bpfdesc bpfdisassembler bpfinfo cfguard codegen codegentypes core coroutines coverage csky cskyasmparser cskycodegen cskydesc cskydisassembler cskyinfo debuginfobtf debuginfocodeview debuginfodwarf debuginfogsym debuginfologicalview debuginfomsf debuginfopdb demangle dlltooldriver dwarflinker dwarflinkerclassic dwarflinkerparallel dwp engine executionengine extensions filecheck frontenddriver frontendhlsl frontendoffloading frontendopenacc frontendopenmp fuzzercli fuzzmutate globalisel hexagon hexagonasmparser hexagoncodegen hexagondesc hexagondisassembler hexagoninfo hipstdpar instcombine instrumentation interfacestub interpreter ipo irprinter irreader jitlink libdriver lineeditor linker loongarch loongarchasmparser loongarchcodegen loongarchdesc loongarchdisassembler loongarchinfo lto m68k m68kasmparser m68kcodegen m68kdesc m68kdisassembler m68kinfo mc mca mcdisassembler mcjit mcparser mips mipsasmparser mipscodegen mipsdesc mipsdisassembler mipsinfo mirparser msp430 msp430asmparser msp430codegen msp430desc msp430disassembler msp430info native nativecodegen nvptx nvptxcodegen nvptxdesc nvptxinfo objcarcopts objcopy object objectyaml option orcdebugging orcjit orcshared orctargetprocess passes powerpc powerpcasmparser powerpccodegen powerpcdesc powerpcdisassembler powerpcinfo profiledata remarks riscv riscvasmparser riscvcodegen riscvdesc riscvdisassembler riscvinfo riscvtargetmca runtimedyld scalaropts selectiondag sparc sparcasmparser sparccodegen sparcdesc sparcdisassembler sparcinfo support symbolize systemz systemzasmparser systemzcodegen systemzdesc systemzdisassembler systemzinfo tablegen target targetparser textapi textapibinaryreader transformutils vectorize webassembly webassemblyasmparser webassemblycodegen webassemblydesc webassemblydisassembler webassemblyinfo webassemblyutils windowsdriver windowsmanifest x86 x86asmparser x86codegen x86desc x86disassembler x86info x86targetmca xray"
 LLVM_FILECHECK="$RUST_DIR/build/$ARCH/ci-llvm/bin/FileCheck"
